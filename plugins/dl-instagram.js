9d2d0214df01251bae17fd20b342213b7d56ab47
RavaAchmad <ravabbok@gmail.com>
2025-10-20 22:24:14 +0700

    Edit instagram

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,12 +1,14 @@
 import fetch from 'node-fetch';
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `*Contoh:* ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
+let handler = async (m, { conn, args, usedPrefix, command, text }) => {
+	if (!args[0]) throw `*Contoh:* ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
 
-    try {
-        const api = await fetch(`https://api.botcahx.eu.org/api/dowloader/igdowloader?url=${args[0]}&apikey=${btc}`)
-        const res = await api.json()
-        
+	if (!text) throw 'Perihal Apah?'
+	var url = text.replace(/\s+/g, '+')
+		try {
+			const api = await fetch(`https://api.botcahx.eu.org/api/dowloader/igdowloader?url=${args[0]}&apikey=${btc}`)
+			const res = await api.json()
+
 			const seen = new Set();
 			const filteredResults = [];
 			for (let item of res.result) {
@@ -21,18 +23,19 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 				await sleep(3000);
 				conn.sendFile(m.chat, filteredResults[i].url, null, `*Instagram Downloader*`, m)
 			}
-    } catch (e) {
-        throw `*Server Down!*`
-    }
-}
+		} catch (e) {
+			m.reply('Server Error!');
+			console.log(e)
+		}
+	}
+
 
 handler.help = ['instagram'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(ig|instagram|igdl|instagramdl|igstroy)$/i
 handler.limit = true
-
 export default handler
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
