95ef405eb48e02a62a531d28cc628d288710d3b4
ravaachmad <ravabbok@gmail.com>
2025-10-28 13:17:32 +0700

    nganu

--- plugins/sticker-iqc.js
+++ plugins/sticker-iqc.js
@@ -29,19 +29,29 @@ let handler = async (m, { conn, text }) => {
   }).format(date)
  
   try {
-    const url = `https://api.deline.my.id/maker/iqc?text=${encodeURIComponent(text.trim())}&chatTime=${encodeURIComponent(chatTime)}&statusBarTime=${encodeURIComponent(statusBarTime)}`
- 
+        let url = `https://api.zenzxz.my.id/maker/fakechatiphone?text=${encodeURIComponent(text.trim())}&chatime=${encodeURIComponent(chatTime)}&statusbartime=${encodeURIComponent(statusBarTime)}`;
+        await conn.sendMessage(
+          m.chat,
+          { image: { url } },
+          { quoted: m }
+        );
+        await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } });
+    
+  } catch (e) {
+    try {
+    const url = `https://api.deline.my.id/maker/iqc?text=${encodeURIComponent(text.trim())}&chatTime=${encodeURIComponent(chatTime)}&statusBarTime=${encodeURIComponent(statusBarTime)}`;
     await conn.sendMessage(
       m.chat,
       { image: { url } },
       { quoted: m }
-    )
- 
+    );
     await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } })
-  } catch (e) {
-    await conn.sendMessage(m.chat, { react: { text: '❌', key: m.key } })
-    throw e
-  }
+        return;
+    } catch (e) {
+    await conn.sendMessage(m.chat, { react: { text: '❌', key: m.key } });
+    throw e;
+    }
+    }
 }
  
 handler.help = ['iqc']
