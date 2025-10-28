54f4a3f7aa631824aace90bbdd57880e57318b4d
ravaachmad <ravabbok@gmail.com>
2025-10-28 12:44:41 +0700

    add : sticker iqc.js

---
+++ plugins/sticker-iqc.js
@@ -0,0 +1,53 @@
+/*• Nama Fitur : Iqc
+• Type : Plugin ESM
+• Link Channel : https://whatsapp.com/channel/0029VbB8WYS4CrfhJCelw33j
+• Author : Agas
+*/
+ 
+ 
+let handler = async (m, { conn, text }) => {
+  if (!text) return m.reply('Example :\n.iqc lu hitam')
+ 
+  await conn.sendMessage(m.chat, { react: { text: '⏳', key: m.key } })
+ 
+  let now = new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' })
+  let date = new Date(now)
+ 
+  let offsetMinutes = Math.floor(Math.random() * (60 - 30 + 1)) + 30
+  let chatDate = new Date(date.getTime() - offsetMinutes * 60000)
+ 
+  let chatTime = new Intl.DateTimeFormat('id-ID', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false
+  }).format(chatDate)
+ 
+  let statusBarTime = new Intl.DateTimeFormat('id-ID', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false
+  }).format(date)
+ 
+  try {
+    const url = `https://api.deline.my.id/maker/iqc?text=${encodeURIComponent(text.trim())}&chatTime=${encodeURIComponent(chatTime)}&statusBarTime=${encodeURIComponent(statusBarTime)}`
+ 
+    await conn.sendMessage(
+      m.chat,
+      { image: { url } },
+      { quoted: m }
+    )
+ 
+    await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } })
+  } catch (e) {
+    await conn.sendMessage(m.chat, { react: { text: '❌', key: m.key } })
+    throw e
+  }
+}
+ 
+handler.help = ['iqc']
+handler.tags = ['maker', 'sticker']
+handler.command = ['iqc']
+handler.register = true
+handler.limit = true
+ 
+export default handler
